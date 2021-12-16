7c7f55299b2f06965b0c157c56b51754798e353a
Steven Díaz Gómez <pibody@hotmail.es>
2021-12-16 02:43:59 -0600

    Se implementa invocación de API para ObtenerTodos y para Crear Nuevo

---
+++ SistemaUniversidad.FrontEnd/Dtos/AulaDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaUniversidad.FrontEnd.Dtos
+{
+    public class AulaDto
+    {
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string NumeroAula { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(30, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string Horario { get; set; }
+
+        public bool Activo { get; set; }
+
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [MaxLength(10, ErrorMessage = "{0} tiene que tener máximo {1} caracteres")]
+        public string CodigoCurso { get; set; }
+    }
+}
